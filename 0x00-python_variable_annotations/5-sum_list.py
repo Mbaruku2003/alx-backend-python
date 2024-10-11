3932bfe850ec9453ebe23ce744454fffe82ce995
Mbaruku2003 <leonardjembe21@gmail.com>
2024-10-11 16:24:09 +0300

    5-sum_list.py

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -5,7 +5,9 @@ from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
+    """we used i as an iterator for the number of values."""
+
     summation = 0.0
-    for i in range (len(input_list)):
+    for i in range(len(input_list)):
         summation += input_list[i]
     return summation
