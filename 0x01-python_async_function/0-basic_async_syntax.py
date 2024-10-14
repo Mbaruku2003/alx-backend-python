7e3ef9397b4f3c854b8f4cf107d8d67502e7804e
Mbaruku2003 <leonardjembe21@gmail.com>
2024-10-14 13:35:43 +0300

    0-basic_async_syntax.py

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -7,4 +7,4 @@ import random
 async def wait_random(max_delay=10):
     """awaits for a random delay between 0 and max_delay."""
 
-    return random.randint(0, max_delay)
+    return await random.randint(0, max_delay)
