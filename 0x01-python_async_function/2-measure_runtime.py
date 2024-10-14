d86602d4b14d07da02f0aeddd2a4208fe47c1597
Mbaruku2003 <leonardjembe21@gmail.com>
2024-10-14 17:42:25 +0300

    completed file creation.

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -6,7 +6,7 @@ import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """returns total_time / n  as a float."""
 
     # start the timer
