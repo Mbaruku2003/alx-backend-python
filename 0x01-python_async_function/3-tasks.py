3233fbcecd7719ff76e37c7e2e85febe7e11d82a
Mbaruku2003 <leonardjembe21@gmail.com>
2024-10-14 18:37:39 +0300

    completed file creation.

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """Takes an integer and returns asyncio.Task."""
 import typing
-wait_random = __import__('0-basic_async_syntax').wait_random
 import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
-    """Takes an integer and returns a asyncio task."""
+    """Takesi an integer and returns a asyncio task."""
 
-    return asyncio.Task(wait_random(max_delay))
+    return asyncio.create_task(wait_random(max_delay))
